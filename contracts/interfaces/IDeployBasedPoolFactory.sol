2600493148b569df0e42e2873ec062a88cdd2833
Nuclaer <dylanbrophy@gmail.com>
2025-10-24 10:41:19 -0500

    Add to the contract interfaces so they are more useful, does not affect functionality

--- contracts/interfaces/IDeployBasedPoolFactory.sol
+++ contracts/interfaces/IDeployBasedPoolFactory.sol
@@ -38,6 +38,14 @@ interface IDeployBasedPoolFactory {
 	/// @return The tick spacing
 	function feeAmountTickSpacing(uint24 fee) external view returns (int24);
 
+	/// @notice Returns the pool address for a given pair of tokens and a fee, or address 0 if it does not exist
+	/// @dev tokenA and tokenB may be passed in either token0/token1 or token1/token0 order
+	/// @param tokenA The contract address of either token0 or token1
+	/// @param tokenB The contract address of the other token
+	/// @param fee The fee collected upon every swap in the pool, denominated in hundredths of a bip
+	/// @return pool The pool address
+	function getPool(address tokenA, address tokenB, uint24 fee) external view returns (address pool);
+
 	function createPool(address reserve, address launch, uint24 fee, uint96 priceMultiple, uint160 sqrtPriceX96, uint96 curveLimit, uint128 reserveOffset, uint128 amount, address newOwner)
 		external returns (address pool);
 }
--- contracts/interfaces/IDeployBasedTokenFactory.sol
+++ contracts/interfaces/IDeployBasedTokenFactory.sol
@@ -5,6 +5,10 @@ pragma solidity ^0.8.17;
 interface IDeployBasedTokenFactory {
 	event TokenCreated(address indexed token, uint8 decimals, string name, string symbol);
 
+	function totalTokens() external view returns (uint256);
+	function tokens(uint256 index) external view returns (address);
+	function poolFactory() external view returns (address);
+
 	function launchToken(string memory name, string memory symbol, uint8 decimals, address reserve, uint24 fee, uint96 priceMultiple, uint160 sqrtPriceX96, uint96 curveLimit, uint128 reserveOffset, uint128 amount)
 		external returns (address token, address pool);
 }
